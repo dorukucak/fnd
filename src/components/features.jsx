caffd8c5e9bc09c010a59e7e5a9f4e4b5b6f5797
Doruk Ucak <dorukucak@live.com>
2022-03-03 20:29:34 +0300

    first

---
+++ src/components/features.jsx
@@ -0,0 +1,23 @@
+export const Features = (props) => {
+  return (
+    <div id='features' className='text-center pt-20'>
+      <div className='container'>
+        <div className='col-md-10 col-md-offset-1 section-title'>
+          <h2>Our Values</h2>
+        </div>
+        <div className='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.title}-${i}`} className='col-xs-6 col-md-3'>
+                  {' '}
+                  <i className={d.icon}></i>
+                  <h3>{d.title}</h3>
+                  <p>{d.text}</p>
+                </div>
+              ))
+            : 'Loading...'}
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/navigation.jsx
@@ -0,0 +1,58 @@
+import logo from './logo.png';
+export const Navigation = (props) => {
+  return (
+    <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
+      <div className='container'>
+        <div className='navbar-header'>
+          <button
+            type='button'
+            className='navbar-toggle collapsed'
+            data-toggle='collapse'
+            data-target='#bs-example-navbar-collapse-1'
+          >
+            {' '}
+            <span className='sr-only'>Toggle navigation</span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+          </button>
+          <img src={logo} alt='logo' height='50' />
+        </div>
+
+        <div
+          className='collapse navbar-collapse'
+          id='bs-example-navbar-collapse-1'
+        >
+          <ul className='nav navbar-nav navbar-right'>
+            <li>
+              <a href='#features' className='page-scroll'>
+                Our Values
+              </a>
+            </li>
+            <li>
+              <a href='#about' className='page-scroll'>
+                About
+              </a>
+            </li>
+            <li>
+              <a href='#services' className='page-scroll'>
+                Services
+              </a>
+            </li>
+            <li>
+              <a href='#portfolio' className='page-scroll'>
+                Gallery
+              </a>
+            </li>
+
+            <li>
+              <a href='#contact' className='page-scroll'>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
